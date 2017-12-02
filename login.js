6fd16e7d7add5d8cce8a460b06a99b2449795439
Alison Moura <alison.oghino@gmail.com>
2017-12-02 20:43:04 -0300

    Refatorando para o inglês

--- login.js
+++ login.js
@@ -1,9 +1,9 @@
 function login(){
     
     var email = document.getElementById("inputEmail").value;
-    var senha = document.getElementById("inputSenha").value;
+    var senha = document.getElementById("inputPassword").value;
 
-    if(email && senha){
+    if(email != "" && senha != ""){
         return true;
     }else{
         window.alert("Digite seu email e senha");
