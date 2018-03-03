4d0bf418b7b2f1d73b23b3ec6f0631c34147f29f
Alison Moura <alison.oghino@gmail.com>
2018-03-03 15:49:48 -0400

    Inserindo NPM e Vuejs no projeto

--- js/controller/clients.controller.js
+++ js/controller/clients.controller.js
@@ -1,57 +1,26 @@
-class ClienteController {
-
-    showForm() {
-        document.querySelector("form").classList.remove("hide");
+var app = new Vue({
+    el: '#app',
+    data: {
+        showForm: false,
+        clients: [
+            {
+                name: "Jão da Silva",
+                email: "jao@gmail.com",
+                cel: "67 99999999",
+                cpf: "897897298743"
+            },
+            {
+                name: "Maria da Silva",
+                email: "mar@gmail.com",
+                cel: "67 89999999",
+                cpf: "837897298743"
+            },
+            {
+                name: "Zé da Silva",
+                email: "ze@gmail.com",
+                cel: "67 99599999",
+                cpf: "893897298743"
+            }
+        ]
     }
-
-    save() {
-
-        var nome = document.getElementById("inputNome").value;
-        var email = document.getElementById("inputEmail").value;
-        var celular = document.getElementById("inputCelular").value;
-        var cpf = document.getElementById("inputCpf").value;
-
-        var tabela = document.getElementById("listTable").querySelector("tbody");
-        var linha = document.createElement("tr");
-
-        var colunaNome = document.createElement("td");
-        var colunaEmail = document.createElement("td");
-        var colunaCelular = document.createElement("td");
-        var colunaCpf = document.createElement("td");
-        var colunaAcao = document.createElement("td");
-
-        var botaoEditar = document.createElement("button");
-        var botaoRemover = document.createElement("button");
-        var botaoDetalhes = document.createElement("button");
-
-        botaoEditar.innerHTML = "Editar";
-        botaoRemover.innerHTML = "Remover";
-        botaoDetalhes.innerHTML = "Detalhes";
-
-        colunaNome.innerHTML = nome;
-        colunaEmail.innerHTML = email;
-        colunaCpf.innerHTML = cpf;
-        colunaCelular.innerHTML = celular;
-
-        colunaAcao.appendChild(botaoEditar);
-        colunaAcao.appendChild(botaoRemover);
-        colunaAcao.appendChild(botaoDetalhes);
-
-        linha.appendChild(colunaNome);
-        linha.appendChild(colunaEmail);
-        linha.appendChild(colunaCelular);
-        linha.appendChild(colunaCpf);
-        linha.appendChild(colunaAcao);
-
-        tabela.appendChild(linha);
-
-        document.querySelector("form").classList.add("hide");
-    }
-
-    cancel() {
-        document.querySelector("form").classList.add("hide");
-    }
-
-}
-
-var clienteController = new ClienteController();
+})
