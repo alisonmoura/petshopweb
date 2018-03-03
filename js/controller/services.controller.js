26fc9975687fc0724d861062ef02a6c9cf74eb52
Pedro Miranda <pedrohmdutraa@hotmail.com>
2018-03-03 17:04:40 -0400

    Trocando a linguagem do services.controller

--- js/controller/services.controller.js
+++ js/controller/services.controller.js
@@ -1,15 +1,29 @@
-class ServicesController {
-    showForm() {
-        document.querySelector("form").classList.remove("hide");
+var app = new Vue({
+    el: '#app',
+    data: {
+        showForm: false,
+        services: [
+            {
+                id: "Jão da Silva",
+                name: "jao@gmail.com",
+                animal: "67 99999999",
+                type: "897897298743",
+                value:"5748538795679"
+            },
+            {
+                id: "Maria da Silva",
+                name: "mar@gmail.com",
+                animal: "67 89999999",
+                type: "837897298743",
+                value:"5748538795679"
+            },
+            {
+                id: "Zé da Silva",
+                name: "ze@gmail.com",
+                animal: "67 99599999",
+                type: "893897298743",
+                value:"5748538795679"
+            }
+        ]
     }
-
-    save() {
-        document.querySelector("form").classList.add("hide");
-    }
-
-    cancel() {
-        document.querySelector("form").classList.add("hide");
-    }
-}
-
-var servicesController = new ServicesController();
+})
